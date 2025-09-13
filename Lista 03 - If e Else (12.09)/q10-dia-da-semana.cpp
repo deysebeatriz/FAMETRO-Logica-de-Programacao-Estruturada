5a06aa216728fc0a7bb09614b26b9c64df01b5a2
Deyse Beatriz <dbfdj.eng20@uea.edu.br>
2025-09-12 20:36:01 -0400

    Q10 - Dia da semana

---
+++ Lista 03 - If e Else (12.09)/q10-dia-da-semana.cpp
@@ -0,0 +1,42 @@
+//Dia da semana
+
+// avaliacao escolar
+
+#include <stdio.h>
+
+int main(){
+	int dia;
+	
+	printf("Digite um dia: ");
+	scanf("%d", &dia);
+	
+	if (dia == 1){
+		printf("Domingo");
+	}
+	else if (dia == 2){
+		printf("Segunda-feira");
+	}
+	else if (dia == 3){
+		printf("Terca-feira");
+	}
+	else if (dia == 4){
+		printf("Quarta-feira");
+	}
+	else if (dia == 5){
+		printf("Quinta-feira");
+	}
+	else if (dia == 6){
+		printf("Sexta-feira");
+	}
+	else if (dia == 7){
+		printf("Sabado");
+	}
+	else {
+		printf("Dia invalido");
+	}
+	
+	
+	return 0;
+}
+
+
